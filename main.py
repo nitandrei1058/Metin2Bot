5d17a98c7e9f7d628ec207473c135d40d0423a56
Nita Andrei <nitandrei1058@gmail.com>
2025-02-05 01:01:03 +0200

    Added all matches functionality

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import cv2 as cv
 import numpy as np
+import os
 
-haystack_img = cv.imread('imagine.jpg', cv.IMREAD_REDUCED_COLOR_2)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+haystack_img = cv.imread('imagine3.jpg', cv.IMREAD_REDUCED_COLOR_2)
 needle_img = cv.imread('pumnal.jpg', cv.IMREAD_REDUCED_COLOR_2)
 
-result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_SQDIFF_NORMED)
 
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+threshold = 0.15
+locations = np.where(result <= threshold)
 
-print('Best match top left position: %s' % str(max_loc))
-print('Best match confidence:  %s' % max_val)
+locations = list(zip(*locations[::-1]))
 
-threshold = 0.8
-if max_val >= threshold:
+if locations:
     print('Found needle.')
 
     needle_w = needle_img.shape[1]
     needle_h = needle_img.shape[0]
+    line_color = (0, 255, 0)
+    line_type = cv.LINE_4
 
-    top_left = max_loc
-    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+    # Loop over all the locations and draw their rectangle
+    for loc in locations:
+        # Determine the box positions
+        top_left = loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        # Draw the box
+        cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
 
-    cv.rectangle(haystack_img, top_left, bottom_right,
-                 color = (0, 255, 0), thickness=2, lineType=cv.LINE_4)
-    
-    cv.imshow('Result', haystack_img)
+    cv.imshow('Matches', haystack_img)
     cv.waitKey()
+    #cv.imwrite('result.jpg', haystack_img)
+
 else:
     print('Needle not found.')
