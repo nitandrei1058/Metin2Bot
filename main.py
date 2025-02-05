17bcc078931757b31258dd25429e791d166e64eb
Nita Andrei <nitandrei1058@gmail.com>
2025-02-05 14:18:29 +0200

    Added HSV features

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from time import time
 from windowcapture import WindowCapture
 from vision import Vision
+from hsvfilter import HsvFilter
 
 # Change the working directory to the folder this script is in.
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
@@ -13,13 +14,12 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # initialize the WindowCapture class
 wincap = WindowCapture('METIN2')
 # initialize the Vision class
-needle = Vision('pumnal.jpg')
+vision_pumnal = Vision('pumnal_hsv2.jpg')
+# initialize the trackbar window
+vision_pumnal.init_control_gui()
 
-'''
-# https://www.crazygames.com/game/guns-and-bottle
-wincap = WindowCapture()
-vision_gunsnbottle = Vision('gunsnbottle.jpg')
-'''
+# limestone HSV filter
+hsv_filter = HsvFilter(18, 143, 190, 115, 255, 255, 147, 0, 0, 0)
 
 loop_time = time()
 while(True):
@@ -27,9 +27,18 @@ while(True):
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
+    # pre-process the image
+    processed_image = vision_pumnal.apply_hsv_filter(screenshot, hsv_filter)
+
+    # do object detection
+    rectangles = vision_pumnal.find(processed_image, 0.5)
+
+    # draw the detection results onto the original image
+    output_image = vision_pumnal.draw_rectangles(screenshot, rectangles)
+
     # display the processed image
-    points = needle.find(screenshot, 0.6, 'rectangles')
-    #points = vision_gunsnbottle.find(screenshot, 0.7, 'points')
+    cv.imshow('Processed', processed_image)
+    cv.imshow('Matches', output_image)
 
     # debug the loop rate
     print('FPS {}'.format(1 / (time() - loop_time)))
